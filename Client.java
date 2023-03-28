871e2e15859fac53604fce1b74840229e81f5d06
46981861 <lucas.ly@students.mq.edu.au>
2023-03-28 22:12:19 +1100

    receive server info

--- Client.java
+++ Client.java
@@ -1,49 +1,65 @@
 import java.io.*;  
 import java.net.*;  
+import java.util.*;
 
 public class Client {
-    public static void readFromBuffer(BufferedReader input) throws IOException {
+    public static String readFromBuffer(BufferedReader input) throws IOException {
         int charNum = input.read();
-        while (charNum != 0){
-            System.out.print((char) charNum);
+        String message = ""; 
+        while (charNum != 10){
+            //System.out.print((char) charNum);
+            message += (char) charNum;
             charNum = input.read();
         }
-        System.out.println();
+        return message;
     }
 
     public static void main(String args[]) {
         try {
-            Socket s = new Socket("localhost",50000);  
+            Socket s = new Socket("localhost", 50000);  
             // Handshaking
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());  
+            BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream dout = new DataOutputStream(new DataOutputStream(s.getOutputStream()));  
+            // SEND HELO
             dout.write(("HELO\n").getBytes());
             dout.flush();  
-
-            readFromBuffer(in);
-
-
+            // RECEIVE OK
+            System.out.println(readFromBuffer(din));
+            // SEND AUTH
             String username = System.getProperty("user.name");
             dout.write(("AUTH " + username + "\n").getBytes());
             dout.flush();
-            readFromBuffer(in);
-            readFromBuffer(in);
-
+            // RECEIVE OK 
+            System.out.println(readFromBuffer(din));
+            // SEND REDY 
             dout.write(("REDY\n").getBytes());
-
-            
-            // Logic
-
-
-            readFromBuffer(in);
-
-            dout = new DataOutputStream(s.getOutputStream()); 
-            dout.write(("BYE\n").getBytes());
             dout.flush();
+            // RECEIVE SOMETHING
+            System.out.println(readFromBuffer(din));
+            // GET SERVER INFO
+            dout.write(("GETS All\n").getBytes());
 
-            readFromBuffer(in);
+            String message = readFromBuffer(din);
+            System.out.println(message);
+            ArrayList<String> array = new ArrayList<>();
+
+            // READ SERVER INFO
+            while (message.equals(".") != true){
+                dout.write(("OK\n").getBytes());
+                array.add(message);
+                message = readFromBuffer(din);
+                //System.out.println(message);
+            }
+
+            System.out.println(array.toString());
+
+            // SEND QUIT
+            dout.write(("QUIT\n").getBytes());
+            dout.flush();
+            // RECEIVE QUIT
+            readFromBuffer(din);
+
             dout.close();
-
             s.close();
 
         } catch(Exception e){
